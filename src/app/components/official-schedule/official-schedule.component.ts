410387c315cd6c75701ecbdd1fc9711cea77f674
Michael Coaguila <maicolfl@gmail.com>
2024-09-19 18:24:02 -0500

    Add login, dashboard, enrollment, enrollment guide, official schedule, schedule simulation, footer, and navbar components

---
+++ src/app/components/official-schedule/official-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-official-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './official-schedule.component.html',
+  styleUrl: './official-schedule.component.css'
+})
+export class OfficialScheduleComponent {
+
+}
