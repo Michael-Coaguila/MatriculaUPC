3027299a7dbc613a1db921877fd81e6ea139b841
Michael Coaguila <maicolfl@gmail.com>
2024-09-19 18:53:55 -0500

    Refactor app component to import necessary modules and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common'; // Para usar ngIf, ngFor, etc.
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './layout/navbar/navbar.component'; // Importar componente de la barra de navegación
+import { FooterComponent } from './layout/footer/footer.component'; // Importar componente del pie de página
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MatriculaUPC';
+
+  // Inyectamos el servicio Router para poder acceder a la URL actual
+  constructor(public router: Router) {}
 }
