a96d14a6e922b99858bd888115f3134700ad14c8
Michael Coaguila <maicolfl@gmail.com>
2024-09-21 00:33:54 -0500

    Refactor dashboard component styles and HTML structure

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Se agrega el módulo CommonModule para poder usar las directivas ngIf y ngFor
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+/* export class DashboardComponent {
+
+} */
+
+export class DashboardComponent implements OnInit {
+
+  alumno: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Simulación de datos del alumno
+    this.alumno = {
+      nombre: 'Michael Coaguila',
+      id: 'U202220780',
+      turno: 'Mañana',
+      tieneDeuda: false, // Cambiar a true para ver cómo se muestra con deuda
+      estaMatriculado: true // Cambiar a true para ver cómo se muestra matriculado
+    };
+  }
 
 }
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+/* export class NavbarComponent {
+
+} */
+
+export class NavbarComponent implements OnInit {
+
+  alumno: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Simulación de datos del alumno
+    this.alumno = {
+      nombre: 'Michael Coaguila',
+      id: 'U202220780',
+      foto: 'https://www.kindpng.com/picc/m/25-254714_graduation-ceremony-vector-graphics-graduate-university-student-icon.png'
+    };
+  }
 
 }
