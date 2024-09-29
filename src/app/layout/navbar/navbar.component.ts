9a6fd0e89a219a36a2e305c6ddf265b1ec15857d
Michael Coaguila <maicolfl@gmail.com>
2024-09-29 14:29:27 -0500

    Refactor navbar component to handle localStorage access safely

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -17,36 +17,47 @@ export class NavbarComponent implements OnInit {
     foto: ''
   };
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    // Extraer los datos del alumno desde localStorage
-    const nombre = localStorage.getItem('estudianteNombre');
-    const id = localStorage.getItem('estudianteID');
-    const codigo = localStorage.getItem('estudianteCodigo');
-    const foto = localStorage.getItem('estudianteFoto');
+    // Verificar si estamos en un entorno de navegador antes de usar localStorage
+    if (this.isBrowser()) {
+      // Extraer los datos del alumno desde localStorage
+      const nombre = localStorage.getItem('estudianteNombre');
+      const id = localStorage.getItem('estudianteID');
+      const codigo = localStorage.getItem('estudianteCodigo');
+      const foto = localStorage.getItem('estudianteFoto');
 
-    // Si hay datos en el localStorage, los asignamos al objeto alumno
-    if (nombre && id && foto) {
-      this.alumno = {
-        nombre,
-        id,
-        codigo,
-        foto
-      };
+      // Si hay datos en el localStorage, los asignamos al objeto alumno
+      if (nombre && id && foto) {
+        this.alumno = {
+          nombre,
+          id,
+          codigo,
+          foto
+        };
+      }
     }
   }
 
   // Método para desconectar al usuario
   logout() {
-    // Eliminar los datos del alumno del localStorage
-    localStorage.removeItem('estudianteNombre');
-    localStorage.removeItem('estudianteID');
-    localStorage.removeItem('estudianteCodigo');
-    localStorage.removeItem('estudianteFoto');
+    if (this.isBrowser()) {
+      // Eliminar los datos del alumno del localStorage
+      localStorage.removeItem('estudianteNombre');
+      localStorage.removeItem('estudianteID');
+      localStorage.removeItem('estudianteCodigo');
+      localStorage.removeItem('estudianteFoto');
+    }
 
     // Redirigir al login
     this.router.navigate(['/login']);
   }
+
+  // Método para verificar si estamos en el navegador
+  isBrowser(): boolean {
+    return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
+  }
 }
+
 
